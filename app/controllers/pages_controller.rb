968a014191eb2237cf08ee4653cb92ae407919b1
Berty Qiming Ruan <berty.ruan@gmail.com>
2013-12-22 16:25:23 -0500

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+
+  def home
+  	#retrieves home.html.erb from views/pages/
+  	#interaction with the database
+  end
+
+  def about
+  	#interaction with the database
+  end
+end
