41d3e1da41079296a529b6f70ae92dd71898f876
Berty Qiming Ruan <berty.ruan@gmail.com>
2013-12-24 21:54:11 -0500

    Finished the Getting Started with Rails Guide: Made a Basic Blog Site

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 
 #This connects you to the database
 class Post < ActiveRecord::Base
-	has_many :comments
+	has_many :comments, dependent: :destroy
 
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :text, presence: true, length: { maximum: 250 }
