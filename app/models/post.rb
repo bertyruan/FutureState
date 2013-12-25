0dc4f09c9d90728d1d92b4bdbc1673dcc25a2a76
Berty Qiming Ruan <berty.ruan@gmail.com>
2013-12-24 19:27:26 -0500

    added Create Read and Update features

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 
 #This connects you to the database
 class Post < ActiveRecord::Base
+	  validates :title, presence: true, length: { minimum: 5 }
+	  validates :text, presence: true, length: { maximum: 250 }
 end
